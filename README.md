489386b106f718c1fbd6ea91393b64eaf6048f7f
MJ Ponsonby <mj@crypticcoder.co.uk>
2021-07-11 05:42:22 +0100

    ini

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Fractal
-makes a fractal using cpp and opengl
-
-the fractal rendered is the triangle one that looks a bit like the tri force from the legend of zelda
-
-done as part of my summer of code project
+# AntSim 
+ant simulation, showing getting food for a nest and optimizing the path used<br>
+coded in c++ and rendered in opengl<br>
+<br>
+done as part of my summer of code project<br>
 [link](https://github.com/crypticC0der/summerofcode)
--- main.cpp
+++ main.cpp
@@ -27,69 +27,20 @@ void nextGenerationSquare(int x1, int y1,float* rgb)
     glEnd();
 }
 */
-const int oneLen=6;
-const int children=4;
-
-float* triangles;
-unsigned int length;
-
-void renderTriangle(int num){
-	glColor3f(0.8,0,0.8);
-	glBegin(GL_LINE_LOOP);
-	for (int i =0;i<oneLen/2;i++){
-		glVertex2d(triangles[num*oneLen + i*2],triangles[num*oneLen+i*2+ 1]);
-	}
-	glEnd();
-}
 
 void disInit(){
 	glClearColor(0.1f, 0.1f, 0.1f, 0); // Set background color to black and opaque
 	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
-	//nextGeneration();
-	for(int i =0;i<length/oneLen;i++){
-		renderTriangle(i);
-	}
 	glFlush();
 }
 
 
 
 void run(){
-	float *newTriangle = new float[length*children];
-	for (int i =0;i<length/oneLen;i++){
-		for(int j=0;j<oneLen/2;j++){
-			if(children>=2){
-				newTriangle[(i*children+j)*oneLen ]=triangles[i*oneLen+j*2];
-				newTriangle[(i*children+j)*oneLen +1]=triangles[i*oneLen+j*2+1];
-			}
-			for(int k=0;k<children-2;k++){
-				newTriangle[((i*children + j)*oneLen) + (k + 1)*2] = (triangles[i*oneLen + (j)*2] + triangles[i*oneLen +((j+k+1)%(oneLen/2))*2])/2;
-				newTriangle[((i*children + j)*oneLen) + (k + 1)*2 +1] = (triangles[i*oneLen + (j)*2 +1] + triangles[i*oneLen +((j+k+1)%(oneLen/2))*2 +1])/2;
-			}
-			newTriangle[(i*children+ children-1)*oneLen +j*2]=(triangles[i*oneLen +j*2] +triangles[i*oneLen+((j+1)%(oneLen/2))*2])/2;
-			newTriangle[(i*children+ children-1)*oneLen +j*2 +1]=(triangles[i*oneLen +j*2 +1] +triangles[i*oneLen+((j+1)%(oneLen/2))*2 +1])/2;
-		}
-	}
-	free(triangles);
-	triangles=newTriangle;
-	length*=children;
-	for(int i=0;i<length/oneLen;i++){
-		renderTriangle(i);
-	}
-	glFlush();
-	sleep(1);
 }
 
 
 int main(int argc, char** argv) {
-	length = oneLen;
-	triangles = new float[oneLen];
-	triangles[0]=-1;
-	triangles[1]=-1;
-	triangles[2]=0;
-	triangles[3]=1;
-	triangles[4]=1;
-	triangles[5]=-1;
 	srand (time(NULL));
 	glutInit(&argc, argv);		// Initialize GLUT
     glutInitWindowSize(600,800);   // Set the window's initial width & height
